baaca2ba5249bf1945b8e8d5b1410872eb1deae5
ItsJulius29 <julio_campos159@outlook.es>
2025-02-11 11:48:55 -0500

    cv-v1.3

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,6 @@
 // Inicializar AOS para animaciones de entrada
 AOS.init({
     duration: 1000, // Duración de las animaciones
-    once: true // Ejecuta la animación solo una vez
+    once: true, // Ejecuta la animación solo una vez
+    offset:200
 });
